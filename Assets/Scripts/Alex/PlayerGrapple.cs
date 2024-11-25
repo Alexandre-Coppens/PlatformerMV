1da698066efe9c0b489248fb752c2bb0e8d11794
alexandre.coppens <alexandre.coppens@artfx.fr>
2024-11-25 11:52:53 +0100

    :construction: [WIP] First draft of the grapple

--- Assets/Scripts/Alex/PlayerGrapple.cs
+++ Assets/Scripts/Alex/PlayerGrapple.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class PlayerGrapple : MonoBehaviour
 {
     public GameObject ropePREFAB;
-    public GameObject endPosition;
 
-    [Range(1, 30)] public int partRopeLength = 5;
+    [Range(0, 5)] public float partRopeLength = 5;
     [Range(1, 30)] public int maxRopeParts = 5;
 
     [SerializeField] private List<GameObject> ropeParts = new List<GameObject>();
+    public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartGrappling(Vector2 startPosition)
     {
-        Vector3 ropeMeasure = Vector3.Normalize(transform.position - endPosition.transform.position);
-        for(int i = 0; i <= ropeLength; i++)
+        transform.position = startPosition;
+        int ropeNbr = Mathf.Clamp((int)(Vector3.Distance(transform.position, player.transform.position) / partRopeLength), 1, maxRopeParts);
+        Vector3 ropeMeasure = Vector3.Normalize(transform.position - player.transform.position);
+        
+        for(int i = 0; i < ropeNbr; i++)
         {
-            GameObject rope = Instantiate(ropePREFAB, transform.position - ropeMeasure * i, Quaternion.identity, transform);
+            GameObject rope = Instantiate(ropePREFAB, transform.position - ropeMeasure * i * partRopeLength, Quaternion.identity, transform);
             if(ropeParts.Count != 0)
             {
                 HingeJoint2D ropeHj = rope.GetComponent<HingeJoint2D>();
@@ -26,11 +28,23 @@ public class PlayerGrapple : MonoBehaviour
             }
             ropeParts.Add( rope );
         }
+
+        player.GetComponent<DistanceJoint2D>().connectedBody = ropeParts[ropeParts.Count - 1].GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void KillGrappling()
     {
-        
+        for (int i = 0; i <= ropeParts.Count; i += 0)
+        {
+            Destroy( ropeParts[ropeParts.Count - 1]);
+        }
+        ropeParts.Clear();
+        player.GetComponent<DistanceJoint2D>().connectedBody = null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRopeParts * partRopeLength);
     }
 }
--- Assets/Scripts/Alex/PlayerPowers.cs
+++ Assets/Scripts/Alex/PlayerPowers.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class PlayerPowers : MonoBehaviour
 {
     [SerializeField] Camera camera;
     [SerializeField] GameObject world;
+    [SerializeField] GameObject grapple;
 
     [SerializeField] private float _speed;
+
+    [SerializeField] private float grappleDist;
 
     private bool canChangeGravity = true;
     private bool gravityInverted = false;
@@ -29,13 +33,8 @@ public class PlayerPowers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // evil floating point bit level hacking
-        if (Input.GetKeyDown(KeyCode.Tab) && canChangeGravity)
-        {
-            InvertGravity();
-        }
-
-        if(newGravity != gravityInverted)
+        KeyInputs();
+        if (newGravity != gravityInverted)
         {
             _rb.velocity = new Vector2(0, -10);
             InvertGravity();
@@ -66,11 +65,40 @@ public class PlayerPowers : MonoBehaviour
         CheckGround();
     }
 
+    private void KeyInputs()
+    {
+        // evil floating point bit level hacking
+        if (Input.GetKeyDown(KeyCode.Tab) && canChangeGravity)
+        {
+            InvertGravity();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 worldPos = Camera.main.WorldToViewportPoint(transform.position);
+            Vector2 mousePos = Camera.main.WorldToViewportPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector2 mouseDirection = (mousePos - worldPos).normalized;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, mouseDirection, grappleDist, groundLayers);
+            Debug.DrawRay(transform.position, mouseDirection * grappleDist, Color.white, 10);
+            if (hit)
+            {
+                GetComponent<DistanceJoint2D>().enabled = true;
+                grapple.GetComponent<PlayerGrapple>().StartGrappling(hit.point);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<DistanceJoint2D>().enabled)
+        {
+            GetComponent<DistanceJoint2D>().enabled = false;
+            grapple.GetComponent<PlayerGrapple>().KillGrappling();
+        }
+    }
+
     public void InvertGravity()
     {
         gravityInverted = !gravityInverted;
         newGravity = gravityInverted;
         world.transform.RotateAround(_rb.worldCenterOfMass + new Vector2(0, -0.1f), new Vector3(0, 0, 1), 180);
+        grapple.transform.RotateAround(_rb.worldCenterOfMass + new Vector2(0, -0.1f), new Vector3(0, 0, 1), 180);
         camera.transform.rotation *= Quaternion.Euler(0, 0, 180);
         gameObject.GetComponent<SpriteRenderer>().flipX = gravityInverted;
         _rb.velocity = new Vector2(_rb.velocity.x, -_rb.velocity.y);
@@ -83,5 +111,11 @@ public class PlayerPowers : MonoBehaviour
         {
             canChangeGravity = true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, grappleDist);
     }
 }
